66253c9c7afc722cbfd0eae5c129147cd92bc3fd
gjuanccamilo <gjuancamilo550@gmail.com>
2026-09-05 16:40:31 +0000

    feat:he agregado la funcionalidad de para imprimir la plabara poción las veces indicadas(3 veces)

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -4,9 +4,13 @@ package com.colegiomagico.reto3;
  * Usa un ciclo for para crear una cadena que repita la palabra 'Pocion ' la cantidad de veces indicada.
  */
 public class PocionCiclos {
-
     public static String preparar(int cantidad) {
-        return ""; // TODO: Cambia esto
+        String nn="";
+     for (int i = 0; i < cantidad; i++) {
+            nn += "Pocion";
+        }
+        
+        return nn;
     }
 
     public static void main(String[] args) {
