5c46e6a2390cc3256d2d4a19721060d5819e04b3
gjuanccamilo <gjuancamilo550@gmail.com>
2026-09-10 01:58:05 +0000

    fix:arregle error en sintaxis del codigo

--- src/main/java/com/colegiomagico/reto8/InventarioPociones.java
+++ src/main/java/com/colegiomagico/reto8/InventarioPociones.java
@@ -1,21 +1,5 @@
 package com.colegiomagico.reto8;
 
-/**
- * Filtra la lista de ingredientes devolviendo solo aquellos que empiezan con 'A'.
- */
-public class InventarioPociones {
-
-    public static java.util.List<String> filtrar(java.util.List<String> ingredientes) {
-        return new java.util.ArrayList<>(); // TODO: Cambia esto
-    }
-
-    public static void main(String[] args) {
-        java.util.List<String> lista = java.util.Arrays.asList("Ajenjo", "Miel");
-        System.out.println("Filtrados: " + filtrar(lista));
-    }
-}
-package com.colegiomagico.reto8;
-
 public class InventarioPociones {
 
     public static java.util.List<String> filtrar(java.util.List<String> ingredientes) {
